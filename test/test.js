0455ecb37e65a74dae203e69a982c3d33e785cf8
mazanlabeeb <mazanlabeeb@gmail.com>
2022-07-04 10:37:33 +0500

    update

---
+++ test/test.js
@@ -0,0 +1,16 @@
+// var Scraper = require('images-scraper');
+
+// const google = new Scraper({
+//   puppeteer: {
+//     headless: true,
+//   },
+// });
+
+// (async () => {
+//   const results = await google.scrape('lana rhodes hot', 5);
+//   console.log(results.length);
+//   console.log('results', results);
+// })();
+let str = "images";
+
+console.log(o)
